21c3126f797c94e37d873a41ef34228d30c0435a
Kuber Budhathoki <koobear99@gmail.com>
2022-07-21 19:18:19 +0545

    Update merge1.2.py

--- merge1.2.py
+++ merge1.2.py
@@ -20,7 +20,7 @@ data2 = [{
     'kpi2':0.65,
     },
     {
-    'district':'kavre',
+    'district':'kavrepalanchowk',
     'kpi2':0.6,
     },
 ]
@@ -48,5 +48,32 @@ margeData(data1, data2)
 print(data3)
 # for fuzzy word
 # using fuzzywuzzy 
-# from fuzzywuzzy import process
-# from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+from fuzzywuzzy import fuzz
+
+def margeFuzzyData(data1, data2):
+    d2s = [ d2['district'] for d2 in data2 ]
+    for d1 in data1:
+        best_match = process.extractOne(d1['district'], d2s, scorer=fuzz.token_sort_ratio)
+        if best_match:
+            d2key =[key for key,value in enumerate(data2) if value['district'] == best_match[0]][0]
+            data3.append(merge(d1, data2[d2key]))
+data3 =[]
+margeFuzzyData(data1, data2)
+print(data3)
+# ----------------------
+# using difflib
+
+import difflib
+
+def margeData(data1, data2):
+    d2s = [ d2['district'] for d2 in data2 ]
+    for d1 in data1:
+        best_match = difflib.get_close_matches(d1['district'], d2s)
+        if best_match:
+            d2key =[key for key,value in enumerate(data2) if value['district'] == best_match[0]][0]
+            data3.append(merge(d1, data2[d2key]))
+
+data3 =[]
+margeData(data1, data2)
+print(data3)
