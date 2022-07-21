2e9e49432989d6375680e064900cf3e26e9c71f3
Kuber Budhathoki <koobear99@gmail.com>
2022-07-21 12:44:46 +0545

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,38 +1,63 @@
 data1 = [{
-    'District':'Kathmandu',
-    'KPI_1':.8,
+    'district':'kathmandu',
+    'kpi1':0.8,
     },
     {
-    'District':'Dhanusha',
-    'KPI_1':.85,
+    'district':'dhanusa',
+    'kpi1':0.85,
     },
     {
-    'District':'Kavrepalanchowk',
-    'KPI_1':.75,
+    'district':'kavre palanchowk',
+    'kpi1':0.75,
     },
 ]
 data2 = [{
-    'District':'Kathmandu',
-    'KPI_2':.35,
+    'district':'kathmandu',
+    'kpi2':0.35,
     },
     {
-    'District':'Dhanusha',
-    'KPI_2':.65,
+    'district':'dhanusha',
+    'kpi2':0.65,
     },
     {
-    'District':'Kavrepalanchowk',
-    'KPI_2':.6,
+    'district':'kavre',
+    'kpi2':0.6,
     },
 ]
 data3 =[]
 def merge(dict1, dict2):
     result = dict1 | dict2 #merge operator (|)  
-    return result  
+    return result 
+
+# assuming the data can have this variety of name already known data
+districtName ={
+    'kathmandu':['ktm', "kathmandu"],
+    'dhanusha':['dhanusha', 'dhanusa'],
+    'kavrepalanchwok':['kavrepalanchowk', 'kavre palanchowk', 'kavre']
+}
+
 def margeData(data1, data2):
     for d1 in data1:
         for d2 in data2:
-            if d1['district'] == d2['district']:
-                data3.append(merge(d1, d2))
-                break            
+            for key, val in districtName.items():
+                if d1['district'] in val and d2['district'] in val:
+                    data3.append(merge(d1, d2))
+                    break
+                
 margeData(data1, data2)
+print(data3)
+# for fuzzy word
+# using fuzzywuzzy 
+from fuzzywuzzy import process
+from fuzzywuzzy import fuzz
+
+def margeFuzzyData(data1, data2):
+    for d1 in data1:
+        d2s = [ d2['district'] for d2 in data2 ]
+        best_match = process.extractOne(d1['district'], d2s, scorer=fuzz.token_sort_ratio)
+        if best_match:
+            d2key =[key for key,value in enumerate(data2) if value['district'] == best_match[0]][0]
+            data3.append(merge(d1, data2[d2key]))
+            
+margeFuzzyData(data1, data2)
 print(data3)
